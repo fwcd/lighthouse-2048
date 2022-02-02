18d8b83b0600c71c3aba6e477e6b7f561591b43e
fwcd <fwcdmail@gmail.com>
2022-02-02 20:37:41 +0100

    Add simple lighthouse app

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Project Lighthouse 2048
+
+A Project Lighthouse implementation of the game 2048.
+
+## Running
+
+First set the following environment variables
+
+```bash
+export LIGHTHOUSE_USERNAME=[your username]
+export LIGHTHOUSE_TOKEN=[your API token]
+```
+
+> For this, you need a valid login at [lighthouse.uni-kiel.de](https://lighthouse.uni-kiel.de).
+
+You can now launch the game by running
+
+```bash
+stack run
+```
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Lighthouse.Connection
+import Lighthouse.Options
+import Lighthouse.Utils.Logging
+import qualified Data.Text as T
+import System.Environment (getEnv)
+
+app :: Listener
+app = mempty
+  { onConnect = do
+      logInfo "app" "Connected!"
+  }
 
 main :: IO ()
-main = someFunc
+main = do
+  username <- T.pack <$> getEnv "LIGHTHOUSE_USERNAME"
+  token    <- T.pack <$> getEnv "LIGHTHOUSE_TOKEN"
+  let auth  = Authentication { authUsername = username, authToken = token }
+      opts  = Options { optAuthentication = auth, optLogHandler = simpleLogHandler infoLevel }
+
+  runLighthouseApp app opts
